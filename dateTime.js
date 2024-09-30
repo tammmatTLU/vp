18899cb0d1ddaa0d2b429de6a7002af621b02f89
Mattias Tamm <tammmatTLU@users.noreply.github.com>
2024-09-30 11:52:28 +0300

    EJS view engine kasutusele võetud.

---
+++ dateTime.js
@@ -0,0 +1,54 @@
+const monthNamesEt = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsmeber"];
+const dayNamesEt = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"];
+
+const dateFormattedEt = function(){
+    var timeNow = new Date();
+    return timeNow.getDate() + ". " + monthNamesEt[timeNow.getMonth()] + " " + timeNow.getFullYear();
+};
+const dayOfWeekEt = function(){
+    var timeNow = new Date();
+    return dayNamesEt[timeNow.getDay()];
+};
+const timeFormattedEt = function(){
+    var timeNow = new Date();
+    return timeNow.getHours() + ":" + timeNow.getMinutes() + ":" + timeNow.getSeconds();
+};
+const partOfDayEt = function() {
+    let dayPart = "suvaline aeg";
+    if (new Date().getDay() > 0 && new Date().getDay() < 5){
+        if (new Date().getHours() < 8){
+            dayPart = "Uneaeg"
+        }
+        else if (new Date().getHours() > 15){
+            dayPart = "Vaba aeg"
+        }
+        else{
+            dayPart = "Kooliaeg"
+        }
+    }
+    else if (new Date().getDay() > 4){
+        if (new Date().getHours() > 1 && new Date().getHours() < 12){
+            dayPart = "Uneaeg"
+        }
+        else if (new Date().getHours() > 11 && new Date().getHours() < 19){
+            dayPart = "Vaba aeg"
+        }
+        else{
+            dayPart = "PIDU!!!"
+        }
+    }
+    else if (new Date().getDay() == 0){
+        if (new Date().getHours() < 2){
+            dayPart = "PIDU!!!"
+        }
+        else if (new Date().getHours() > 1 && new Date().getHours() < 12){
+            dayPart = "Uneaeg"
+        }
+        else{
+            dayPart = "Puhkeaeg"
+        }
+    }
+    return dayPart;
+};
+//ekspordin kõik vajaliku
+module.exports = {dateFormatted: dateFormattedEt, dayOfWeek: dayOfWeekEt, timeFormatted: timeFormattedEt, partOfDay: partOfDayEt, monthNames: monthNamesEt, dayNames: dayNamesEt};
--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const express = require("express");
+const dateTime = require("./dateTime");
+const fs = require("fs")
+
+
 const app = express();
+//määran view mootori (ejs)
+app.set("view engine", "ejs");
+//määran jagatavate avalike failide kausta
+app.use(express.static("public"));
 
 app.get("/", (req,res)=>{
-    res.send("Express läks käima!");
+    //res.send("Express läks käima!");
+    res.render("index");
+});
+
+app.get("/timenow", (req,res)=>{
+    const dateNow = dateTime.dateFormattedEt();
+    const timeNow = dateTime.timeFormattedEt();
+    const dayOfWeekNow = dateTime.dayOfWeekEt();
+    res.render("timenow", {nowWD: dayOfWeekNow, nowD: dateNow, nowT: timeNow});
+});
+
+app.get("/vanasonad", (req,res)=>{
+    let folkWisdom = [];
+    fs.readFile("public/textfiles/vanasonad.txt", "utf8", (err, data)=>{
+        if(err){
+            res.render("justlist", {h2: "Vanasõnu ei ole", listData: []});
+        }
+        else{
+            folkWisdom = data.split(";");
+            res.render("justlist", {h2: "Vanasõnad", listData: folkWisdom});
+        }
+    });
 });
 
 app.listen(5209);
